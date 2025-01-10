748d142d4c0dabbec5bfe9c60ddb305746a96516
irannagkk <hejjegariranna@gmail.com>
2025-01-10 17:21:04 +0530

    spring jenkins demo

---
+++ src/test/java/com/iranna/SpringbootDemoApplicationTests.java
@@ -0,0 +1,22 @@
+package com.iranna;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJenkinsApplicationTest {
+	  public static Logger logger = LoggerFactory.getLogger(SpringbootJenkinsApplicationTest.class);
+
+	@Test
+	void contextLoads() {
+		logger.info("Test case executing");
+		assertEquals(true,true);
+	}
+	
+
+}
